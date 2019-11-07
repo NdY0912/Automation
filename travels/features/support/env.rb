60b432c1754d7a659ddfab8586b22dea7011a97c
NdY0912 <53474140+NdY0912@users.noreply.github.com>
2019-11-07 14:00:15 +0530

    Delete env.rb

--- travels/features/support/env.rb
+++
@@ -1,8 +0,0 @@
-require 'selenium-webdriver'
-
-Selenium::WebDriver::Firefox::Service.driver_path="C:/Users/sparepcs/Documents/Webdriver/geckodriver.exe"
-
-Before do
-  $driver = Selenium::WebDriver.for :firefox
-  $driver.get "https://www.phptravels.net/register/"
-end
